7b0d1f7b97cf4bb8d44a4bd0729855c2b91fc764
Charles McLeod <charlesmcleod@usf.edu>
2025-02-06 20:47:19 -0500

    Task 2 - Order Details

--- cc_5.js
+++ cc_5.js
@@ -10,3 +10,21 @@ const customer = {
 console.log('Name: ${customer.name}');
 console.log('Age: ${customer.age}');
 console.log('Email: ${customer.email}');
+
+//Task 2
+//Declare the object order
+const order = {
+    orderId: 47308,
+    totalAmount: 275,
+    status: "Processing",
+
+    //Add the method displayOrder()
+    displayOrder: function() {
+        console.log('Order ID: ${this.orderId}');
+        console.log('Total Amount: $${.totalAmount}');
+        console.log('Status: ${this.status}');
+    }
+};
+
+//Call the method and log the details
+order.displayOrder();
