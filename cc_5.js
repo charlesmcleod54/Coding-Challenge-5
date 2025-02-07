8c31a1ae584f5c750a7a1f1435a52106c39726c3
Charles McLeod <charlesmcleod@usf.edu>
2025-02-06 20:41:02 -0500

    Task 1 - Customer Profile

---
+++ cc_5.js
@@ -0,0 +1,12 @@
+// Task 1
+// Declare the customer object
+const customer = {
+    name: "Chuck Jones",
+    age: 25,
+    email: "chuckj25@gmail.com"
+};
+
+//Log each property 
+console.log('Name: ${customer.name}');
+console.log('Age: ${customer.age}');
+console.log('Email: ${customer.email}');
